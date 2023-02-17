c850b438d284b0e4f87faa1dda432d7b16aa5e08
yashupadhyaya <uyash73@gmail.com>
2023-02-17 14:40:18 +0530

    url

--- RoostTest/cypresstest/cypress/integration/cypresstest.spec.js
+++ RoostTest/cypresstest/cypress/integration/cypresstest.spec.js
@@ -1,6 +1,6 @@
 describe('Microservices Demo Frontend', () => {
     it('should be up and running', () => {
-      cy.request(Cypress.env('frontend')).then(response => {
+      cy.request(Cypress.env('ROOST_SVC_URL')).then(response => {
         expect(response.status).to.eq(200);
         expect(response.body).to.include('Sock Shop');
       });
